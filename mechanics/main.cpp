1c1e7c491dfb0639377f994c282ee936be34f2a0
ChillGonzales <cmmonroe2010@gmail.com>
2021-01-17 09:13:10 -0500

    stopping at the section for loading texture images

--- mechanics/main.cpp
+++ mechanics/main.cpp
@@ -1,10 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
 
 int main()
 {
@@ -32,6 +32,52 @@ int main()
 	// Register a callback for when the user resizes the window to tell open GL the new window size
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// With our nice shader class, just give the paths and call use
+	Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl");
+	shader.use();
+
+	// Indexed drawing using EBO's
+	float vertices[] = {
+	 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right + color
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right + color
+	-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left + color
+	-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f  // top left + color
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	float texCoords[] = {
+		0.0f, 0.0f, // lower left
+		1.0f, 0.0f, // bottom right
+		0.0f, 1.0f, // top left
+		1.0f, 1.0f
+	};
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. copy our element array 
+	// NOTE: Our EBO gets bound to our VAO automatically by OpenGL
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	// 4. then set our vertex attributes pointers
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -39,6 +85,19 @@ int main()
 		// Rendering logic
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Activate our shader program
+		shader.use();
+
+		//float timeValue = glfwGetTime();
+		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		// Render the rectangle (using indexed drawing and EBO)
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
