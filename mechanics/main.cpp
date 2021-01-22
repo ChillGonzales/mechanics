2a055fc9443b7474a5c59c22b7b4b1fdb9f058dd
ChillGonzales <cmmonroe2010@gmail.com>
2021-01-21 21:52:38 -0500

    you're darn right we're loading models!

--- mechanics/main.cpp
+++ mechanics/main.cpp
@@ -7,6 +7,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <assimp/scene.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
