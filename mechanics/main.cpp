d8e7367066830ccd5603ec10306998497415279b
ChillGonzales <cmmonroe2010@gmail.com>
2021-01-29 21:21:16 -0500

    workin on physics

--- mechanics/main.cpp
+++ mechanics/main.cpp
@@ -22,15 +22,16 @@ void processInput(GLFWwindow* window);
 Vector3 toPhysVec(glm::vec3 vec);
 
 // settings
-const int _width = 1000;
-const int _height = 700;
+const int SCR_WIDTH = 1000;
+const int SCR_HEIGHT = 700;
+const int NUM_OBJECTS = 7;
 const float _physicsTimestep = 1.0f / 60.0f;
 bool enablePhysics = false;
 
 // camera
 Camera camera(glm::vec3(0.0f, 5.0f, 15.0f));
-float lastX = _width / 2.0f;
-float lastY = _height / 2.0f;
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
 // timing
@@ -40,6 +41,24 @@ float lastFrame = 0.0f;
 // positions
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
+struct Meshes
+{
+	Meshes()
+	{ }
+
+	// Rendering
+	Model* models[NUM_OBJECTS];
+	glm::mat4 model_mats[NUM_OBJECTS];
+
+	// Physics
+	//Transform transforms[NUM_OBJECTS];
+	Transform prev_transforms[NUM_OBJECTS];
+	//Vector3 positions[NUM_OBJECTS];
+	//Quaternion rotations[NUM_OBJECTS];
+	RigidBody* bodies[NUM_OBJECTS];
+	Collider* colliders[NUM_OBJECTS];
+};
+
 int main()
 {
 	glfwInit();
@@ -47,7 +66,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(_width, _height, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -68,7 +87,7 @@ int main()
 		return -1;
 	}
 
-	glViewport(0, 0, _width, _height);
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 	glEnable(GL_DEPTH_TEST);
 
 	// Register a callback for when the user resizes the window to tell open GL the new window size
@@ -79,9 +98,6 @@ int main()
 	Shader lightShader("shaders/light/vertex.glsl", "shaders/light/fragment.glsl");
 	Shader lampShader("shaders/lamp/vertex.glsl", "shaders/lamp/fragment.glsl");
 	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-
-	Model backpack("assets/backpack/backpack.obj");
-
 
 	glm::vec3 pointLightPositions[] = {
 		glm::vec3(0.7f,  0.2f,  2.0f),
@@ -103,6 +119,17 @@ int main()
 		lightShader.setVec3("pointLights[" + s + "].specular", 1.0f, 1.0f, 1.0f);
 	}
 
+	// Set up our objects for physics and rendering
+	Meshes meshes;
+	
+	auto ball = Model("assets/ball/ball.obj");
+	meshes.models[0] = &ball;
+	for (unsigned int i = 1; i <= 6; i++)
+	{
+		auto floor = Model("assets/plank/plank.obj");
+		meshes.models[i] = &floor;
+	}
+
 	// **** PHYSICS SETUP ****
 	// Create the world settings 
 	PhysicsWorld::WorldSettings settings;
@@ -117,34 +144,54 @@ int main()
 	world->setNbIterationsPositionSolver(8);
 
 	// Rigidbody setup
-	glm::vec3 backpackPos = glm::vec3(0.0f, 10.0f, 0.0f);
-	glm::vec3 floorPos = glm::vec3(0.0f, 0.0f, 0.0f);
-	Transform backpackTrans = Transform(toPhysVec(backpackPos), Quaternion::identity());
-	Transform floorTrans = Transform(toPhysVec(floorPos), Quaternion::identity());
-	RigidBody* backpackRBody = world->createRigidBody(backpackTrans);
-	RigidBody* floorRBody = world->createRigidBody(floorTrans);
-	backpackRBody->setType(BodyType::DYNAMIC);
-	floorRBody->setType(BodyType::STATIC);
-
-	// Collider setup
-	// Instantiate a sphere collision shape 
-	float radius = 1.0f;
+	auto ballTransform = Transform(Vector3(0.0f, 10.0f, 0.0f), Quaternion::identity());
+	meshes.prev_transforms[0] = ballTransform;
+	auto rBody = world->createRigidBody(ballTransform);
+	rBody->setType(BodyType::DYNAMIC);
+	float radius = 3.0f;
 	SphereShape* sphereShape = common.createSphereShape(radius);
 	BoxShape* boxShape = common.createBoxShape(Vector3(10.0f, 1.0f, 10.0f));
 
 	// Relative transform of the collider relative to the body origin 
-	Transform transform = Transform::identity();
+	Transform ident = Transform::identity();
 
 	// Add the collider to the rigid body 
-	Collider* backpackCollider = backpackRBody->addCollider(sphereShape, transform);
-	Collider* floorCollider = floorRBody->addCollider(boxShape, transform);
-	backpackCollider->getMaterial().setBounciness(0.6f);
+	auto collider = rBody->addCollider(sphereShape, ident);
+	collider->getMaterial().setBounciness(0.8f);
+	meshes.colliders[0] = collider;
+
+	Vector3 angles[] = {
+		Vector3(0.0f, 0.0f, 0.0f),
+		Vector3(0.0f, 0.0f, 0.0f),
+		Vector3(90.0f, 0.0f, 0.0f),
+		Vector3(90.0f, 0.0f, 0.0f),
+		Vector3(0.0f, 0.0f, 90.0f),
+		Vector3(0.0f, 0.0f, 90.0f)
+	};
+	Vector3 positions[] = {
+		Vector3(0.0f, 0.0f, 0.0f),
+		Vector3(0.0f, 30.0f, 0.0f),
+		Vector3(0.0f, 0.0f, -15.0f),
+		Vector3(0.0f, 0.0f, 15.0f),
+		Vector3(-15.0f, 0.0f, 0.0f),
+		Vector3(15.0f, 0.0f, 0.0f)
+	};
+	for (unsigned int i = 1; i <= 6; i++)
+	{
+		auto trans = Transform(positions[i-1], Quaternion::fromEulerAngles(angles[i-1]));
+		//meshes.transforms[i] = trans;
+		meshes.prev_transforms[i] = trans;
+		auto rBody = world->createRigidBody(trans);
+		rBody->setType(BodyType::STATIC);
+		meshes.bodies[i] = rBody;
+		auto coll = rBody->addCollider(boxShape, ident);
+		meshes.colliders[i] = coll;
+	}
 
 	// Init variables for main loop
 	float accumulator = 0.0f;
 	float modelMatrix[16];
 	float factor = 0.0f;
-	Transform prevTransform = backpackRBody->getTransform();
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -167,15 +214,15 @@ int main()
 			// Compute the time interpolation factor 
 			decimal factor = accumulator / _physicsTimestep;
 
-			// Get the updated transform of the body 
-			Transform currTransform = backpackRBody->getTransform();
-			//std::cout << "Current position: " << currTransform.getPosition().to_string() << std::endl;
+			for (unsigned int i = 0; i < NUM_OBJECTS; i++)
+			{
+				auto currTrans = meshes.bodies[i]->getTransform();
+				// Compute the interpolated transform of the rigid body 
+				Transform interpolatedTransform = Transform::interpolateTransforms(meshes.prev_transforms[i], currTrans, factor);
 
-			// Compute the interpolated transform of the rigid body 
-			Transform interpolatedTransform = Transform::interpolateTransforms(prevTransform, currTransform, factor);
-
-			// Update the previous transform 
-			prevTransform = currTransform;
+				// Update the previous transform 
+				meshes.prev_transforms[i] = currTrans;
+			}
 		}
 
 		// Rendering logic
@@ -203,25 +250,30 @@ int main()
 		lightShader.setFloat("spotLight.quadratic", 0.032f);
 
 		// pass projection matrix to shader (note that in this case it could change every frame)
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)_width / (float)_height, 0.1f, 100.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		lightShader.setMat4("projection", projection);
 
 		// camera/view transformation
 		glm::mat4 view = camera.GetViewMatrix();
 		lightShader.setMat4("view", view);
-		backpackRBody->getTransform().getOpenGLMatrix(modelMatrix);
-		glm::mat4 model = glm::make_mat4(modelMatrix);
-		//model = glm::translate(model, trans.getPosition()); // add the translation from our source of truth translation to the model matrix
-		//model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-		lightShader.setMat4("model", model);
-		backpack.Draw(lightShader);
-
+		for (unsigned int i = 0; i < NUM_OBJECTS; i++)
+		{
+			meshes.prev_transforms[i].getOpenGLMatrix(modelMatrix);
+			glm::mat4 model = glm::make_mat4(modelMatrix);
+			//model = glm::translate(model, trans.getPosition()); // add the translation from our source of truth translation to the model matrix
+			//model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+			lightShader.setMat4("model", model);
+			meshes.models[i]->Draw(lightShader);
+		}
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	// Clean up physics memory
-	world->destroyRigidBody(backpackRBody);
+	for (unsigned int i = 0; i < NUM_OBJECTS; i++)
+	{
+		world->destroyRigidBody(meshes.bodies[i]);
+	}
 	common.destroyPhysicsWorld(world);
 
 	glfwTerminate();
--- mechanics/model.h
+++ mechanics/model.h
@@ -142,6 +142,12 @@ private:
             for (unsigned int j = 0; j < face.mNumIndices; j++)
                 indices.push_back(face.mIndices[j]);
         }
+
+        if (!mesh->mMaterialIndex)
+        {
+            return Mesh(vertices, indices, textures);
+        }
+
         // process materials
         aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
         // we assume a convention for sampler names in the shaders. Each diffuse texture should be named
